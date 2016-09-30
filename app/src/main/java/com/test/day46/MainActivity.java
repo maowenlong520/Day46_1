0d010e13071497f2d7ac17f6e4ac1de70af296b3
aa <1747956712@qq.com>
2016-09-30 16:44:46 +0800

    initData()

---
+++ app/src/main/java/com/test/day46/MainActivity.java
@@ -0,0 +1,21 @@
+package com.test.day46;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
+    }
+}
